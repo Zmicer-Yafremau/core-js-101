059842fd28ced2a2e1098b033486950ed8822475
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2022-11-30 23:04:21 +0300

    refactor: solve 95/3

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -66,8 +66,15 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (!args.length) return null;
+  return function poly(x) {
+    let res = 0;
+    for (let i = 0; i < args.length; i += 1) {
+      res += args[i] * x ** (args.length - 1 - i);
+    }
+    return res;
+  };
 }
 
 
@@ -136,8 +143,13 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function (...args) {
+    logFunc(`${func.name}(${JSON.stringify(args).slice(1).slice(0, -1)}) starts`);
+    const res = func(...args);
+    logFunc(`${func.name}(${JSON.stringify(args).slice(1).slice(0, -1)}) ends`);
+    return res;
+  };
 }
 
 
